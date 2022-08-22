863e478a3c547e2df57ba5ef2229adad0ec55122
Olivia <ramsfol@gmail.com>
2022-08-22 09:47:11 -0500

    created models

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,13 +5,13 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
+  //TODO: find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+  //TODO: find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
@@ -90,7 +90,7 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  //TODO: delete one product by its `id` value
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,25 +4,107 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  //TODO: find all tags
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    // be sure to include its associated Product data
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price'],
+        //not sure if this should be included?
+        through: ProductTag,
+        as: 'product_tag'
+      }
+    ],
+    order: ['DESC']
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  //TODO: find a single tag by its `id`
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    //might need to include a sequelize.literal in attributes below:
+    attributes: ['id', 'tag_name'],
+    // be sure to include its associated Product data
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price'],
+        //not sure if this should be included?
+        through: ProductTag,
+        as: 'product_tag'
+      }
+    ],
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id.'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  //TODO: create a new tag
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  //TODO: update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id.' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  //TODO: delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id.' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
